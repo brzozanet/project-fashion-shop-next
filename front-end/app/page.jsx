ab46f371ff9b691f4495afffeec67f16559d6244
Paweł Brzozowski <pawel@brzoza.net>
2025-09-05 13:54:31 +0200

    Home

--- front-end/app/page.jsx
+++ front-end/app/page.jsx
@@ -1,18 +1,26 @@
+import { Bestsellers } from "./components/Bestsellers/Bestsellers";
 import { Hero } from "./components/Hero/Hero";
+import { GENDERS_MAPPING } from "./constants/mappings";
 
 export default async function Home() {
   const BACKEND_URL = process.env.BACKEND_URL;
   let genderProducts;
+  let favouriteProducts;
 
   try {
     const gender = "women";
-    const response = await fetch(`${BACKEND_URL}/${gender}`);
+    // const gender = GENDERS_MAPPING.get(params.gender);
+    const [genderResponse, favouriteResponse] = await Promise.all([
+      await fetch(`${BACKEND_URL}/${gender}`),
+      await fetch(`${BACKEND_URL}/favourites`),
+    ]);
 
-    if (!response.ok) {
+    if (!genderResponse.ok || !favouriteResponse.ok) {
       throw new Error(`Błąd HTTP, ${response.status}`);
     }
 
-    genderProducts = await response.json();
+    genderProducts = await genderResponse.json();
+    favouriteProducts = await favouriteResponse.json();
   } catch (error) {
     console.error("Błąd pobierania danych", error);
     return <div>Błąd pobierania danych</div>;
@@ -21,6 +29,10 @@ export default async function Home() {
   return (
     <>
       <Hero imageUrl={genderProducts.heroImageUrl} />
+      <Bestsellers
+        bestsellersData={genderProducts.bestsellers}
+        favouritesData={favouriteProducts}
+      />
     </>
   );
 }
